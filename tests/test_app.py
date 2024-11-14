27cb1c245d6986ea1d09637fb9daba541e75e4b9
Taline Chehade <tmc11@mail.aub.edu>
2024-11-14 19:55:21 +0200

    fixing mistake in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ from app import greet
 
 class TestApp(unittest.TestCase):
     def test_greet(self):
-        self.assertEqual(greet("World"), "Hello, World from Taline Chehade!")
+        self.assertEqual(greet("World"), "Hello, World!")
 
 if __name__ == "__main__":
     unittest.main()
